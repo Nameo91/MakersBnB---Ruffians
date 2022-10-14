7413d071f5212d73312423e4a446dc635e90d404
Thomas Allen <tomallen@192.168.1.149>
2022-10-14 12:03:04 +0100

    image functionality

--- lib/space.rb
+++ lib/space.rb
@@ -1,7 +1,7 @@
 require 'sinatra/activerecord'
 
 class Space < ActiveRecord::Base
-  validates_presence_of :space_name, :price_per_night, :description
+  validates_presence_of :space_name, :price_per_night, :description, :image
 
   has_many :requests
   belongs_to :users
